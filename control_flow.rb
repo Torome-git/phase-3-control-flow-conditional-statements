8752eaff7bccb2e64e33e07369dee21757c115c4
soila.torome <soila.torome@student.moringaschool.com>
2022-10-25 19:19:50 +0300

    completed

--- control_flow.rb
+++ control_flow.rb
@@ -1,16 +1,54 @@
 def admin_login(username, password)
   # your code here
+  if (username == "admin" || username == "ADMIN") && password == "12345"
+    return "Access granted"
+  else
+    return "Access denied"
+  end
 end
 
 def hows_the_weather(temperature)
   # your code here
+  if temperature < 40
+    "It's brisk out there!"
+  elsif temperature > 40 && temperature < 65
+    "It's a little chilly out there!"
+  elsif temperature > 85
+    "It's too dang hot out there!"
+  else
+    "It's perfect out there!"
+  end
 end
 
 def fizzbuzz(num)
   # your code here
+  remainder1 = num % 3
+  remainder2 = num % 5
+  if remainder1 == 0 && remainder2 == 0
+    "FizzBuzz"
+  elsif remainder1 == 0 
+    "Fizz"
+  elsif remainder2 == 0
+    "Buzz"
+  else
+    num
+  end
+
 end
 
 def calculator(operation, num1, num2)
   # your code here
+  case operation
+  when "+"
+    num1 + num2
+  when "-"
+    num1 - num2
+  when "*"
+    num1 * num2
+  when "/"
+    num1 / num2
+  else
+    puts "Invalid operation!"
+  end
 end
 
